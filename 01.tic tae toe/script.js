5f7ecd1b71131b759c7aa65615352a99c2c4f029
unknown <julietagopa2@gmail.com>
2020-09-10 18:11:48 -0500

    mensaje

--- 01.tic tae toe/script.js
+++ 01.tic tae toe/script.js
@@ -1,15 +1,58 @@
 let turnoPrimerJugador = true
 
-function marcarCasilla() {
-  let casilla = document.getElementById('casilla1');
-  casilla.classList.add("morada");
+function marcarCasilla(numero) {
+  let casilla = document.getElementById('casilla'+ numero);
 
+let  ocupada = estaOcupada(casilla)
+if(!ocupada){
   if (turnoPrimerJugador) {
-        casilla.childNodes[0].innerText ="X";
-        turnoPrimerJugador = false
+      casilla.classList.add("morada");
+      casilla.classList.remove("verde")
+      casilla.childNodes[0].innerText ="X";
+      turnoPrimerJugador = false
       }
-    else {
+          else {
+      casilla.classList.add("verde");
+      casilla.classList.remove("morada")
     casilla.childNodes[0].innerText="O";
-    turnoPrimerJugador = true
+        turnoPrimerJugador = true
     }
+}
+}
+
+
+
+function estaOcupada(casilla) {
+if (casilla.childNodes[0].innerText) {
+  console.log("si esta ocupada")
+return true;
+} else {
+  console.log("no esta ocupada")
+  return false;
+
+
+}
+}
+
+
+
+function reiniciarJuego() {
+limpiarCasilla(1);
+limpiarCasilla(2);
+limpiarCasilla(3);
+limpiarCasilla(4);
+limpiarCasilla(5);
+limpiarCasilla(6);
+limpiarCasilla(7);
+limpiarCasilla(8);
+limpiarCasilla(9);
+}
+function limpiar(numero) {
+let casilla = document.getElementById("casilla"+ numero);
+//vaciar casilla o texto vacio
+casilla.childNodes[0].innerText="";
+//quitamos estilos
+casilla.classList.remove("morada");
+casilla.classList.remove("verde");
+
 }
