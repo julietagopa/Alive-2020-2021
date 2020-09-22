12f6abfe35d5929dd21891aae476de17b4d1844a
unknown <julietagopa2@gmail.com>
2020-09-22 16:20:06 -0500

    adelantos

--- 01.tic tae toe/script.js
+++ 01.tic tae toe/script.js
@@ -1,4 +1,5 @@
 let turnoPrimerJugador = true
+let copiaTablero=[]
 
 function marcarCasilla(numero) {
   let casilla = document.getElementById('casilla'+ numero);
@@ -9,32 +10,67 @@ if(!ocupada){
       casilla.classList.add("morada");
       casilla.classList.remove("verde")
       casilla.childNodes[0].innerText ="X";
+      copiaTablero[numero-1]='x'
       turnoPrimerJugador = false
       }
           else {
       casilla.classList.add("verde");
       casilla.classList.remove("morada")
     casilla.childNodes[0].innerText="O";
+      copiaTablero[numero-1]='o'
         turnoPrimerJugador = true
     }
-}
+  if ( revisarGanador()){
+    console.log("YA HAY UN GANADOR")
+    //BLOQUEAR TABLERO
+    //MOSTRAR LETRERO DE FLCDS
+     }
+   }
+
 }
 
+function revisarGanador() {
+  //revisar todas las posibles opciones de ganar la partida dependiendo de la posisicion de mis arreglos
+
+if (
+( copiaTablero[0] && copiaTablero[0] == copiaTablero[3] && copiaTablero[0] == copiaTablero[6]) ||
+(copiaTablero[1] && copiaTablero[1] == copiaTablero[4] && copiaTablero[1] == copiaTablero[7])||
+(copiaTablero[2] && copiaTablero[5] == copiaTablero[2] && copiaTablero[2] == copiaTablero[8])
+) {
+  return true;
+
+}
+//horizontales
+if (
+( copiaTablero[0] && copiaTablero[0] == copiaTablero[1] && copiaTablero[0] == copiaTablero[2]) ||
+(copiaTablero[3] && copiaTablero[3] == copiaTablero[4] && copiaTablero[3] == copiaTablero[5])||
+(copiaTablero[6] && copiaTablero[6] == copiaTablero[7] && copiaTablero[6] == copiaTablero[8])
+) {
+  return true;
+
+}
+if (
+( copiaTablero[0] && copiaTablero[0] == copiaTablero[4] && copiaTablero[0] == copiaTablero[8]) ||
+(copiaTablero[2] && copiaTablero[2] == copiaTablero[4] && copiaTablero[2] == copiaTablero[6])
+) {
+
+return true;
+}
+ return false;
+}
 
 
 function estaOcupada(casilla) {
 if (casilla.childNodes[0].innerText) {
-  console.log("si esta ocupada")
+
 return true;
 } else {
-  console.log("no esta ocupada")
+
   return false;
 
 
 }
 }
-
-
 
 function reiniciarJuego() {
 limpiarCasilla(1);
@@ -46,6 +82,8 @@ limpiarCasilla(6);
 limpiarCasilla(7);
 limpiarCasilla(8);
 limpiarCasilla(9);
+
+copiaTablero=[];
 }
 function limpiar(numero) {
 let casilla = document.getElementById("casilla"+ numero);
@@ -54,5 +92,4 @@ casilla.childNodes[0].innerText="";
 //quitamos estilos
 casilla.classList.remove("morada");
 casilla.classList.remove("verde");
-
 }
